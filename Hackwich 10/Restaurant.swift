eeaede5b7008c928ce98ef53387da8585986835b
Cassandra K <ckk30@hawaii.edu>
2023-11-09 13:10:31 -1000

    completed map and says American still

--- Hackwich 10/Restaurant.swift
+++ Hackwich 10/Restaurant.swift
@@ -6,7 +6,24 @@
 //
 
 import UIKit
-
-class Restaurant: NSObject {
-
+import MapKit
+//part 4
+class Restaurant: NSObject, MKAnnotation {
+    let restaurantTitle: String?
+    let restaurantType: String?
+    var coordinate: CLLocationCoordinate2D
+    init(title: String?, type: String?, coordinate: CLLocationCoordinate2D) 
+    {
+        self.restaurantTitle = title
+        self.restaurantType = type
+        self.coordinate = coordinate
+        super.init()
+        
+    }
+    //part 5
+    var subtitle: String?
+    {
+        //Problem Set #3 Code Switch
+        return restaurantType
+    }
 }
--- Hackwich 10/ViewController.swift
+++ Hackwich 10/ViewController.swift
@@ -12,12 +12,33 @@ import CoreLocation
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
-   
+    
+    //part 3
+    let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.056022)
+    
+    let regionRadius = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centerMapOnLocation(location: initialLocation)
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American",
+                       coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude:
+                       -158.080098))
+                
+                mapView.addAnnotation(restaurantOne)
+        
     }
+//part 3
+    func centerMapOnLocation(location:CLLocation)
+           {
+               let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
 
+               latitudinalMeters: CLLocationDistance(regionRadius),
+               longitudinalMeters: CLLocationDistance(regionRadius))
+               mapView.setRegion(coordinateRegion, animated: true)
 
+           }
+    
 }
 
